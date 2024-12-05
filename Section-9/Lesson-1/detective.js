ae20a540c60065867a88c523c4ec5d2ac8b02f84
Jessi Zimmerman <jessi_leeann@yahoo.com>
2024-12-04 19:33:06 -0800

    completed

--- Section-9/Lesson-1/detective.js
+++ Section-9/Lesson-1/detective.js
@@ -1,5 +1,7 @@
-function mysteryOperation ()
-{
+// try operationSucess { +13 days vacay } catch { +1 day vacay }
+// finally { +3 days vacay }
+
+function mysteryOperation (){
 	const outcome = Math.random(); // Generates a random number between 0 and 1.
 
 	if (outcome < 0.5)
@@ -11,3 +13,18 @@ function mysteryOperation ()
 		throw new Error("The operation is failed mysteriously!");
 	}
 }
+
+let totalMissions = 20;
+var vacationDays = 0;
+for (let i = 0; i < totalMissions; i++){
+	try {
+		mysteryOperation();
+		vacationDays += 13;
+	} catch (error){
+		vacationDays += 1;
+	} finally {
+		vacationDays += 3;
+	}
+}
+
+console.log(vacationDays);
