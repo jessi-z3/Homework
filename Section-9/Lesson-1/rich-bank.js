43b5c82caa997e82438555d8a8034f4ade0bb35f
Jessi Zimmerman <jessi_leeann@yahoo.com>
2024-12-05 16:07:44 -0800

    finished

--- Section-9/Lesson-1/rich-bank.js
+++ Section-9/Lesson-1/rich-bank.js
@@ -3,55 +3,85 @@ const accounts = [
 	{id: 2, owner: "Bob", balance: 300}
 ];
 
-function getAccountById (id)
-{
-	for (const account of accounts)
-	{
-		if (account.id == id)
-		{
+function getAccountById (id){
+	let accountFound = Boolean
+	// Added check for postive number
+	if (!Number.isFinite(id) || id < 0){
+			throw new Error("The account ID must be a postive finite number.");
+	}
+
+	for (const account of accounts){
+		if (account.id == id){
+			accountFound = true;
 			return account;
-		}
+		} 
+	}
+
+	// An account hasn't been found or we would have returned it and exited
+	accountFound = false
+
+	// Added check for not finding an account here
+	if (!accountFound) {
+		throw new Error("Account not found.")
 	}
 }
 
-function createAccount (newAccountId, newAccountOwner)
-{
-	accounts.push(
-		{
-			id: newAccountId,
-			owner: newAccountOwner,
-			balance: "0"
+function createAccount (newAccountId, newAccountOwner){
+	// Added check for positive number
+	if (!Number.isFinite(newAccountId) || newAccountId < 0) {
+		throw new Error("The account ID must be a finite number.");
+	}
+
+	// Added check for string and length
+	if (typeof newAccountOwner === "string" ) {
+		const trimmed = newAccountOwner.trim();
+		if (trimmed.length === 0) {
+			throw new Error("Account owner cannot be an empty string.");
 		}
-	);
+	} else {
+		throw new Error("The account name must be a string.");
+	}
+
+	accounts.push({
+		id: newAccountId,
+		owner: newAccountOwner,
+		// Resolved a TypeError here, where it was a Sting, causing concatenation
+		balance: 0
+	});
 }
 
-function depositMoney (accountId, amount)
-{
+function depositMoney (accountId, amount) {
 	const account = getAccountById(accountId);
 
-	if (!account)
-	{
+	if (!account){
 		throw new Error("Account not found");
+	}
+
+	// Added check for positive number
+	if (!Number.isFinite(amount) || amount <= 0){
+		throw new Error("Invalid value for deposit amount: The amount must be a positive finite number.");
 	}
 
 	account.balance += amount;
 }
 
-function withdrawMoney (accountId, amount)
-{
+function withdrawMoney (accountId, amount){
 	const account = getAccountById(accountId);
-
-	if (!account)
-	{
+	if (!account){
 		throw new Error("Account not found.");
 	}
 
-	if (!Number.isFinite(amount))
-	{
-		throw new Error("Invalid value for withdrawal amount: The amount must be a finite number.");
+	// Added check for positive number
+	if (!Number.isFinite(amount) || amount <= 0){
+		throw new Error("Invalid value for withdrawal amount: The amount must be a positive finite number.");
 	}
 
-	account.balance -= amount;
+	// Added check for funds
+	if (account.balance >= amount) {
+		account.balance -= amount;
+	} else {
+		throw new Error("Insufficient funds.");
+	}
 }
 
 function transferMoney (fromAccountId, toAccountId, amount)
@@ -59,42 +89,18 @@ function transferMoney (fromAccountId, toAccountId, amount)
 	const fromAccount = getAccountById(fromAccountId);
 	const toAccount = getAccountById(toAccountId);
 
-	if (!fromAccount)
-	{
-		throw new Error("Source account not found.");
-	}
-
-	if (!Number.isFinite(amount) || amount < 0)
+	// Added check for positive number
+	if (!Number.isFinite(amount) || amount <= 0)
 	{
 		throw new Error("Invalid value for transfer amount: The amount must be a positive finite number.");
 	}
 
-	toAccount.balance += amount;
+	// Added balance check 
+	if (fromAccount.balance >= amount) {
+		// The balance was not being deducted by the amount previously.
+		fromAccount.balance -= amount;
+		toAccount.balance += amount;
+	} else {
+		throw new Error("Insufficient funds.");
+	}
 }
-
-/*
-Hints:
-
-getAccountById("1");
-
-createAccount(1, "Alice");
-createAccount("3", "Charlie");
-createAccount(-3, "Charlie");
-createAccount(3, ["Charlie"]);
-createAccount(3, "");
-createAccount(3, "  ");
-
-depositMoney(1, "300")
-depositMoney(1, -300)
-depositMoney(1, 0)
-depositMoney(1, Infinity)
-depositMoney(4, 100)
-
-withdrawMoney(1, -100)
-withdrawMoney(1, 0)
-withdrawMoney(1, 501)
-
-transferMoney(1, 4, 100)
-transferMoney(1, 2, 501);
-transferMoney(1, 2, 100);
-*/
