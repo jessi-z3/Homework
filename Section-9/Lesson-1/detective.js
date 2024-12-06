df57f0183d1b53f1c63a9876115c74d768724949
Jessi Zimmerman <jessi_leeann@yahoo.com>
2024-12-05 16:09:08 -0800

    cleaned up

--- Section-9/Lesson-1/detective.js
+++ Section-9/Lesson-1/detective.js
@@ -1,6 +1,3 @@
-// try operationSucess { +13 days vacay } catch { +1 day vacay }
-// finally { +3 days vacay }
-
 function mysteryOperation (){
 	const outcome = Math.random(); // Generates a random number between 0 and 1.
 
@@ -26,5 +23,3 @@ for (let i = 0; i < totalMissions; i++){
 		vacationDays += 3;
 	}
 }
-
-console.log(vacationDays);
